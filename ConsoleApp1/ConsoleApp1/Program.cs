50cd3ce5daf0093f7a38762603c646521fa0125d
Andre Paul Vigneault <apvg1990@hotmail.com>
2019-10-19 21:35:08 -0600

    Ejemplo Regex
    Ejemplo Regex

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una tarjeta de credito");
+            string texto = Console.ReadLine();
+            Regex miRegex = new Regex(@"^\d{4}[ -]?\d{4}[ -]?\d{4}[ -]?(\d{4})$");
+            if (miRegex.IsMatch(texto))
+            {
+                var match = miRegex.Match(texto);
+                Console.WriteLine($"Los ultimos cuatro numeros son: {match.Groups[1]}");
+            }
+            else {
+                Console.WriteLine("No es una tarjeta");
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
